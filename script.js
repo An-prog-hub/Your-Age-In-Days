9e24497e4bb0d18549c979af4e2d16c2b7935d43
Ankush Chauhan <67872399+An-prog-hub@users.noreply.github.com>
2020-08-11 14:26:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Age in Days
+
+function ageInDays() {
+var birthYear = prompt("Which Year Did You Born Dear....??");
+var ageInDayss = (2020 - birthYear) * 365;
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode('You Are ' + ageInDayss + ' Days Old.')
+h1.setAttribute('id', 'ageInDays');
+h1.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendChild(h1);    
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
